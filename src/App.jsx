5ecdd59a31482f94547f657739ddfc7f0941cc39
capzlock35 <capzlock35@gmail.com>
2024-09-08 00:27:27 +0800

    xxx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+ 
+  let A = `Hello World`
+  return (
+
+    <div>
+      <div className='text-xl font-bold text-red-500 bg-blue-600'>
+        {A}
+      </div>
+    </div>
+  )
+}
+
+export default App
